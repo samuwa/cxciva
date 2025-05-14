cdc71c912df0ca2e4eb29042aecf0f6f9dbc8492
samuwa <69830704+samuwa@users.noreply.github.com>
2025-05-14 12:04:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def process_excel(df):
         'BRILUX_Automercados': df[(df['COMPAÑIA'] == 'FABRICA BRILUX C.A.') & (df['Customer Name'].str.contains('AUTOMERCADOS PLAZA', na=False))],
         'EXTRUVENSO_no_Specified': df[(df['COMPAÑIA'] == 'FABRICA EXTRUVENSO C.A.') & (~df['Customer Name'].str.contains('FERRETOTAL|CENTROBECO|FERRETERIA EPA', na=False))],
         'EXTRUVENSO_Specified': df[(df['COMPAÑIA'] == 'FABRICA EXTRUVENSO C.A.') & (df['Customer Name'].str.contains('FERRETOTAL|CENTROBECO|FERRETERIA EPA', na=False))],
-        'MAXPACK_Specified': df[(df['COMPAÑIA'] == 'GRUPO MAXPACK C.A.') & (df['Customer Name'].str.contains('FERRETOTAL|CENTROBECO|FERRETERIA EPA', na=False))],
+        'MAXPACK_no_Specified': df[(df['COMPAÑIA'] == 'GRUPO MAXPACK C.A.') & (~df['Customer Name'].str.contains('FERRETOTAL|CENTROBECO|FERRETERIA EPA', na=False))],
         'MAXPACK_Specified': df[(df['COMPAÑIA'] == 'GRUPO MAXPACK C.A.') & (df['Customer Name'].str.contains('FERRETOTAL|CENTROBECO|FERRETERIA EPA', na=False))]
         
     }
